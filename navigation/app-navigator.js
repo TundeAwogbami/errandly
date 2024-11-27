07d147354bfa2b233aeafee5f6f1e2bf5e0d1ffa
Awogbami Tunde <oladipupo.jayden@gmail.com>
2024-11-27 10:46:21 +0100

    Modified navigation

--- navigation/app-navigator.js
+++ navigation/app-navigator.js
@@ -1,48 +1,48 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import LoadingScreen from "./src/screens/LoadingScreen";
-import SplashScreen from "./src/screens/SplashScreen";
-import SignupScreen from "./src/screens/SignupScreen";
-import WelcomeScreen from "./src/screens/WelcomeScreen";
-import CreateOrderScreen from "./src/screens/CreateOrderScreen";
-import TrackOrderScreen from "./src/screens/TrackOrderScreen";
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoadingScreen from '../screens/LoadingScreen';
+import SplashScreen from '../screens/SplashScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import SignupScreen from '../screens/SignupScreen';
+import CreateOrderScreen from '../screens/CreateOrderScreen';
+import TrackOrderScreen from '../screens/TrackOrderScreen';
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
-      <Stack.Navigator initialRouteName="LoadingScreen">
-         <Stack.Screen
-          name="Loading"
-          component={LoadingScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Splash"
-          component={SplashScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={SignupScreen}
-          options={{ headerShown: false }}
-        />
-        
-        <Stack.Screen
-          name="Welcome"
-          component={WelcomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="CreateOrder"
-          component={CreateOrderScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="TrackOrder"
-          component={TrackOrderScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+    <Stack.Navigator initialRouteName="Loading">
+      <Stack.Screen 
+        name="Loading" 
+        component={LoadingScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="SplashScreen" 
+        component={SplashScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="Welcome" 
+        component={WelcomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="Signup" 
+        component={SignupScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="CreateOrder" 
+        component={CreateOrderScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="TrackOrder" 
+        component={TrackOrderScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
   );
 }
+
